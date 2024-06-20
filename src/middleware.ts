fa9dfbdb4ea5fc7841f748c5504d7be5ab7372f9
Alexandre Bedel <alexandre.bedel@epitech.eu>
2024-06-20 21:19:00 +0200

    Fixed cookie issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,15 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const isAdmin = localStorage.getItem("admin");
-  if (isAdmin && isAdmin === "true" && !request.url.includes("/admin")) {
-    return NextResponse.redirect(new URL("/admin", request.url));
+  const isAdmin = cookies().get("admin");
+
+  if (!isAdmin || isAdmin.value !== "true") {
+    return NextResponse.redirect(new URL("/", request.url));
   }
-  return NextResponse.redirect(new URL("/", request.url));
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/admin",
 };
